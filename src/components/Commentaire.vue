808e480de8a138227c6eb456e146400690c12bb7
Thom-fs <thomas.schneider29@outlook.fr>
2022-05-20 18:48:18 +0200

    commentaires post

---
+++ src/components/Commentaire.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input
+      v-model="postCommentaire"
+      type="text"
+      placeholder="Ecrivez votre commentaire"
+    />
+    <button @click="publish">Publier</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
